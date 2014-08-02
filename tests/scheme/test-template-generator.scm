0c1235d09cb4ca045cc77858a8b8c5005aa2752e
Jay Osako <JosephOsako@gmail.com>
2014-08-01 22:48:47 -0400

    Moved test to right location.

--- tests/scheme/test-template-generator.scm
+++ tests/scheme/test-template-generator.scm
@@ -1,5 +1,30 @@
 #!r6rs
-(import ((rnrs base (6))
-         (bootstrap template-generator))
- 
- (my-or))
+(import (rnrs base (6))
+        (rnrs io simple (6))
+	(assiah bootstrap template-generator))
+
+(write '(foo "bar" quux))
+(newline)
+
+(define-field-pattern foo-0 (width 3)
+  ((default => 0)))
+(write foo-0)
+(newline)
+
+(define-field-pattern foo-1 (width 3)
+  ((bar => 2)))
+(write foo-1)
+(newline)
+
+(define-field-pattern foo-2 (width 3)
+  (("bar" => 2)))
+(write foo-2)
+(newline)
+
+
+(define-field-pattern foo-3 (width 3)
+  ((bar "baz" quux => 2)
+   (bar => 1)
+   (default => 0)))
+(write foo-3)
+(newline)
